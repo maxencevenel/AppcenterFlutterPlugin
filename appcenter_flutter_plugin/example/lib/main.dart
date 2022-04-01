d6bd7eaca0fd8547765c822d7be9b7c9adae41a3
Maxence VENEL <maxence.venel@gmail.com>
2022-04-01 16:38:02 +0200

    fix

--- appcenter_flutter_plugin/example/lib/main.dart
+++ appcenter_flutter_plugin/example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:appcenter_flutter_plugin/appcenter_flutter_plugin.dart';
 
 void main() {
@@ -16,8 +15,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
@@ -26,42 +23,21 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion = "Unknown";
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      await AppCenter.startAsync(
-        appSecretAndroid: "e46523bf-3fb3-43ef-b8f6-93125b1cbfc8",
-        appSecretIOS: "1c7e6ff1-8b96-4b3f-ace3-967869ec996e",
-        enableAnalytics: true, // Defaults to true
-        enableCrashes: true, // Defaults to true
-      );
+    await AppCenter.startAsync(
+      appSecretAndroid: "**********",
+      appSecretIOS: "**********",
+      enableAnalytics: true, // Defaults to true
+      enableCrashes: true, // Defaults to true
+    );
 
-      // await AppCenter.trackErrorAsync("test Error");
-      // await AppCenter.trackErrorAsync("test Error With properties", {"KeyErrorTest":"ValueErrorTest"} );
-      // AppCenter.trackEventAsync('my package event', <String, String> {
-      //   'Key': 'Value',
-      //   'Key1': 'Value1',
-      // });
+    // await AppCenter.trackErrorAsync("test Error");
+    // await AppCenter.trackErrorAsync("test Error With properties", {"KeyErrorTest":"ValueErrorTest"} );
+    // AppCenter.trackEventAsync('my package event', <String, String> {
+    //   'Key': 'Value',
+    //   'Key1': 'Value1',
+    // });
 
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    var a = [12];
-    var test = a[3];
-    throw Exception("coucou");
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -70,8 +46,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: const Center(
+          child: Text('AppCenter plugin'),
         ),
       ),
     );
